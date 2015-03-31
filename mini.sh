280b6cc9e61bdb6e3d8bed1799972766c47d4606
George Hotz <geohot@gmail.com>
2015-03-30 22:44:42 -0700

    exmaple data

--- mini.sh
+++ mini.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-time stdbuf -i0 -o0 -e0 ../minisat/minisat_blbd/binary/minisat_blbd formula_fg | tee out
-
+#time stdbuf -i0 -o0 -e0 ../minisat_blbd/minisat_blbd/code/simp/minisat_release formula_fg | tee out
+time stdbuf -i0 -o0 -e0 ../minisat/core/minisat_release formula_fg | tee out
 
